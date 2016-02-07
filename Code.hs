5bca43d2ca69ae1cc349828d35a6d9d3d5cca924
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-02-07 01:35:49 +0000

    Add code validation.
    Fix #10.

--- Code.hs
+++ Code.hs
@@ -1,4 +1,7 @@
-module Code (CodeWithUnknowns, CodeWithoutUnknowns) where
+module Code (
+    CodeWithUnknowns, CodeWithoutUnknowns, Bit,
+    known, unknown, used, unused, CodeValidation (..), validate
+    ) where
 
 type BoolWithUnknowns = Maybe Bool
 type Bit a = Maybe a
@@ -10,8 +13,29 @@ type CodeWithoutUnknowns = [Bit Bool]
 known :: Bool -> Bit BoolWithUnknowns
 known value = Just (Just value)
 
-unused :: Bit BoolWithUnknowns
+unused :: Bit a
 unused = Nothing
 
-unknonwn :: Bit BoolWithUnknowns
-unknonwn = Just Nothing
+used :: Bool -> Bit Bool
+used value = Just value
+
+unknown :: Bit BoolWithUnknowns
+unknown = Just Nothing
+
+data CodeValidation = Valid
+                    | LengthMismatch
+                    | UnusedBitRemoved
+                    | UnusedBitAdded
+                    | KnownBitChanged
+
+validate :: CodeWithUnknowns -> CodeWithoutUnknowns -> CodeValidation
+validate []     []     = Valid
+validate []     _      = LengthMismatch
+validate _      []     = LengthMismatch
+validate (a:as) (b:bs)
+    | a == unused      = (b /= unused    ) `thenError` UnusedBitRemoved
+    | a == unknown     = (b == unused    ) `thenError` UnusedBitAdded
+    | a == known True  = (b /= used True ) `thenError` KnownBitChanged
+    | a == known False = (b /= used False) `thenError` KnownBitChanged
+  where
+    condition `thenError` result = if condition then result else validate as bs
