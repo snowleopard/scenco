0889d1726b48c942d1cb804ea0715d506abc7892
AlessandrodeGennaro <a.de-gennaro@ncl.ac.uk>
2016-05-17 15:48:50 +0100

    Remove redundant brackets in Library

--- src/Tuura/Library.hs
+++ src/Tuura/Library.hs
@@ -14,7 +14,7 @@ loadLibrary = Library
 libCheck :: Library -> IO ()
 libCheck lib = do
     libPresent <- isLibPresent lib
-    when (libPresent == False) . putStrLn $ ("Gate library " ++ show (libraryFile lib) ++ " not found.")
+    when (libPresent == False) . putStrLn $ "Gate library " ++ show (libraryFile lib) ++ " not found."
 
 isLibPresent :: Library -> IO Bool
 isLibPresent lib = doesFileExist (libraryFile lib)
