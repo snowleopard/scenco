686429b44febe32235e68fb24bb756d81de5f039
AlessandrodeGennaro <a.de-gennaro@ncl.ac.uk>
2016-05-17 15:47:24 +0100

    Redundant brackets removed

--- src/Tuura/Abc.hs
+++ src/Tuura/Abc.hs
@@ -10,6 +10,6 @@ abcCheck = do
     maybePath <- findExecutable abcCommand
     case maybePath of
         Just _  -> return ()
-        Nothing -> error $ (  "ABC synthesis tool not found.\n"
+        Nothing -> error $    "ABC synthesis tool not found.\n"
                            ++ "Read README file for the instructions on "
-                           ++ "how to install and set ABC in the system path.")
+                           ++ "how to install and set ABC in the system path."
