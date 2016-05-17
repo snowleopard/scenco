2b5371b15971c5af4f1d0298e6044225506d3c45
AlessandrodeGennaro <a.de-gennaro@ncl.ac.uk>
2016-05-17 15:27:10 +0100

    Various corrections

--- src/Tuura/Abc.hs
+++ src/Tuura/Abc.hs
@@ -1,17 +1,15 @@
 module Tuura.Abc (abcCommand, abcCheck) where
 
 import System.Directory
-import Control.Monad
 
 abcCommand :: FilePath
 abcCommand = "abc"
 
 abcCheck :: IO ()
 abcCheck = do
-    abcInstalled <- isAbcPresent
-    when (abcInstalled == False) . error $ ("ABC synthesis tool not found.\n"
-                                         ++ "Read README file for the instructions on "
-                                         ++ "how to install and set ABC in the system path.")
-
-isAbcPresent :: IO Bool
-isAbcPresent = doesFileExist abcCommand
+    maybePath <- findExecutable abcCommand
+    case maybePath of
+        Just _  -> return ()
+        Nothing -> error $ (  "ABC synthesis tool not found.\n"
+                           ++ "Read README file for the instructions on "
+                           ++ "how to install and set ABC in the system path.")
