6dc1adc826a5fe592ea7a378499a361c9fdbba66
Alessandro de Gennaro <allegroCoder@users.noreply.github.com>
2016-05-17 16:06:16 +0100

    Check for Abc and for the gate library (#28)
    * Check for Abc and for the gate library
    
    * Code refactoring
    
    * appveyor fix
    
    * Various corrections
    
    * import of Test sorted
    
    * Redundant brackets removed
    
    * Remove redundant brackets in Library

--- Test.hs
+++ Test.hs
@@ -1,19 +1,31 @@
-import Tuura.Scenco
+import Data.List
+import System.Directory
+import System.FilePath
+import System.IO
+
+import Tuura.Abc
 import Tuura.Code
 import Tuura.Graph
 import Tuura.Library
-
-import System.FilePath
+import Tuura.Scenco
 
 testPath :: FilePath
 testPath = "test"
 
-techLibPath :: FilePath
-techLibPath = (testPath </> "90nm.genlib")
+techLib :: Library
+techLib = loadLibrary (testPath </> "90nm.genlib")
+
+-- automatic generation
+nGraphs :: Int
+nGraphs = 120
+
+nEvents :: Int
+nEvents = 5
 
 main :: IO ()
 main = do
     putStrLn scencoVersion
+    abcCheck
     executeTests
 
 executeTests :: IO ()
@@ -25,6 +37,7 @@ executeTests = do
     testIntel9
     testTexasInstrument7
     testTexasInstrument8
+    testAutomaticGeneration nGraphs nEvents
 
 testArm8 :: IO ()
 testArm8 = do
@@ -61,17 +74,53 @@ testTexasInstrument8 = do
     putStrLn "========== Texas Instrument MSP 430 (8 Partial orders)"
     runTests "TI_MSP_430_8" "TI_MSP_430_8"
 
+testAutomaticGeneration :: Int -> Int -> IO ()
+testAutomaticGeneration g e = do
+    randGraphs <- generateGraphs g e
+    let graphs  = loadGraph    randGraphs
+        codes   = getCodesFile ""
+    runEncodings graphs codes
+    removeFile randGraphs
+
 runTests :: FilePath -> FilePath -> IO ()
 runTests cpogPath codesSetPath = do
     let codesPath   = (testPath </> codesSetPath <.> "opcodes")
         codes       = getCodesFile codesPath
         graphsPath  = (testPath </> cpogPath <.> "cpog")
         graphs      = loadGraph graphsPath
-        techLib     = loadLibrary techLibPath
-    codeConstraints <- parseCustomCode codes
-    _ <- loadGraphsAndCodes graphs codes
+    runEncodings graphs codes
+
+runEncodings :: GraphsFile -> CodesFile -> IO ()
+runEncodings graphs codes = do
+    codeConstraints <- loadGraphsAndCodes graphs codes
     _ <- sequentialSearch graphs techLib CPOG
     _ <- singleLiteralSearch graphs techLib CPOG
     _ <- randomSearch graphs techLib codeConstraints 10 CPOG
     _ <- heuristicSearch graphs techLib codeConstraints 10 CPOG
     unloadGraphs
+
+generateGraphs :: Int -> Int -> IO (FilePath)
+generateGraphs ng ne = do
+    putStrLn ("========== Random graphs generation ("
+             ++ show ng ++ " Partial orders - " ++ show ne ++ " Events)")
+    let graphsNumber = take ng $ permutations [1..ne]
+    putStrLn (show (length graphsNumber) ++ " graphs generated.")
+    (tempPath,tempHandle) <- openTempFile testPath "g.cpog"
+    hPutStr tempHandle (convertIntoScenarios 1 (convertIntoStrings graphsNumber))
+    hClose tempHandle
+    return (tempPath)
+
+convertIntoStrings :: [[Int]] -> [String]
+convertIntoStrings []     = []
+convertIntoStrings (x:xs) = convertIntoString x : convertIntoStrings xs
+
+convertIntoString :: [Int] -> String
+convertIntoString []     = "\n"
+convertIntoString (e:es) = "event_" ++ show e ++ " " ++ convertIntoString es
+
+convertIntoScenarios :: Int -> [String] -> String
+convertIntoScenarios _ [] = ""
+convertIntoScenarios n (x:xs) =   (".scenario " ++ "graph_" ++ show n ++ "\n")
+                               ++ x
+                               ++ ".end\n\n"
+                               ++ convertIntoScenarios (n+1) xs
--- src/Tuura/Abc.hs
+++ src/Tuura/Abc.hs
@@ -1,4 +1,15 @@
-module Tuura.Abc (abcCommand) where
+module Tuura.Abc (abcCommand, abcCheck) where
+
+import System.Directory
 
 abcCommand :: FilePath
 abcCommand = "abc"
+
+abcCheck :: IO ()
+abcCheck = do
+    maybePath <- findExecutable abcCommand
+    case maybePath of
+        Just _  -> return ()
+        Nothing -> error $    "ABC synthesis tool not found.\n"
+                           ++ "Read README file for the instructions on "
+                           ++ "how to install and set ABC in the system path."
